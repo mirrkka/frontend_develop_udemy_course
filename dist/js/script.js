31196f64d324bdc6ac0fc669a410e43e0ea1c404
Sasha-Sokolova <mashapiterskaya@yandex.ru>
2021-12-10 00:03:17 +0300

    hide and show modal

--- dist/js/script.js
+++ dist/js/script.js
@@ -106,4 +106,48 @@ function setClock(selector, endtime){
 
 setClock('.timer', deadline);
 
+
+// Modal
+
+const btn = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalClose = document.querySelector('[data-close]');
+
+
+btn.forEach(button => {
+    button.addEventListener('click', () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    });
+
+});
+
+
+function closeModal(){
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+};
+
+
+
+modalClose.addEventListener('click', closeModal);
+
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
+
+    }
+});
+
+document.addEventListener('keydown', (e)=> {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+        closeModal();
+    }
+
+})
+
+
 });
