e7b7e464dd36ab5ecde97d44cffbdfed9bb16da9
Sasha-Sokolova <mashapiterskaya@yandex.ru>
2021-12-11 22:43:29 +0300

    class

--- dist/js/script.js
+++ dist/js/script.js
@@ -147,7 +147,71 @@ document.addEventListener('keydown', (e)=> {
         closeModal();
     }
 
-})
+});
+
+
+// классы для карточек
+/* class User {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHi() {
+    alert(this.name);
+  }
+
+}
+
+// Использование:
+let user = new User("Иван");
+user.sayHi();*/
+
+
+class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.parent = document.querySelector(parentSelector);
+        this.price = price;
+        this.transfer = 27;
+        this.changetoUAH
+    }
+
+    changetoUAH() {
+        this.price = this.price * this.transfer;
+    }
+
+    render () {
+
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        </div>`;
+        this.parent.append(element);
+
+
+    }
+}
+
+new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+).render();
 
 
 });
